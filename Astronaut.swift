324046eb8e489f2a92354119824d73227abad734
Anzhelika Sokolova <63107259+Sokolang@users.noreply.github.com>
2021-01-28 21:58:09 +0200

    Add files via upload

---
+++ Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot2
+//
+//  Created by Anzhellika Sokolova on 28.01.2021.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ Moonshot2App.swift
@@ -0,0 +1,17 @@
+//
+//  Moonshot2App.swift
+//  Moonshot2
+//
+//  Created by Anzhellika Sokolova on 28.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct Moonshot2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
